45640d390c4e077fc5fb69893ae856bfa012d117
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2025-12-01 11:30:28 +0100

    Added code for lesson 3

---
+++ Lektion-03/eShop/domain.tests/ProductTest.cs
@@ -0,0 +1,30 @@
+﻿namespace domain.tests;
+
+public class ProductTests
+{
+    [Fact]
+    public void ShouldCreateProductInstanceWithCorrectInitializing()
+    {
+        // Arrange...
+        var product = new Product(itemNumber: "AA-12345", name: "Spikpistol", price: 4995);
+
+        // Act...
+
+        // Assert...
+        Assert.IsType<Product>(product);
+    }
+
+    [Fact]
+    public void ShouldHaveProperties()
+    {
+        // Arrange...
+        var product = new Product(itemNumber: "AA-12345", name: "Spikpistol", price: 4995);
+
+        // Act...
+
+        // Assert...
+        Assert.Equal("AA-12345", product.ItemNumber);
+        Assert.Equal("Spikpistol", product.Name);
+        Assert.Equal(4995, product.Price);
+    }
+}
---
+++ Lektion-03/eShop/domain/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace domain;
+
+public class Product(string itemNumber, string name, decimal price)
+{
+    public string ItemNumber { get; set; } = itemNumber;
+    public string Name { get; set; } = name;
+    public decimal Price { get; set; } = price;
+}
