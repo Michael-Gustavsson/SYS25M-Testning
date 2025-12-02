36296b214f096e14a4ec968a6755a61ff435ee9d
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2025-12-02 09:59:07 +0100

    Added lesson 4

---
+++ Lektion-04/eShop/domain.tests/fixtures/ProductFixtures.cs
@@ -0,0 +1,16 @@
+﻿namespace domain.tests;
+
+public class ProductFixture : IDisposable
+{
+    public Product Product { get; set; }
+
+    public ProductFixture()
+    {
+        Product = new Product(itemNumber: "AA-12345", name: "Spikpistol", price: 4995);
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ Lektion-04/eShop/domain/Product.cs
@@ -0,0 +1,36 @@
+﻿namespace domain;
+
+public class Product(string itemNumber, string name, decimal price)
+{
+    public string ItemNumber { get; set; } = itemNumber;
+    public string Name { get; set; } = name;
+    public decimal Price { get; set; } = price;
+
+    public void Update(string itemNumber, string name, decimal price)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentException("Namn saknas", nameof(name));
+        }
+
+        if (name.Length < 5)
+        {
+            throw new ArgumentException("Namn måste vara minst 5 tecken");
+        }
+
+        if (string.IsNullOrEmpty(itemNumber))
+        {
+            throw new ArgumentException("Artikelnummer saknas");
+        }
+
+        if (itemNumber.Length < 7)
+        {
+            throw new ArgumentException("Artikelnummer måste vara minst 7 tecken");
+        }
+
+        if (price < 0)
+        {
+            throw new ArgumentException("Pris kan inte vara lägre än 0");
+        }
+    }
+}
