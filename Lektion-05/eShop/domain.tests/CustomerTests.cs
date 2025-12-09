62722f72aca2e8779ca103f3ee5b947a1f9a1b58
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2025-12-09 08:23:50 +0100

    Added starter for lesson 5

---
+++ Lektion-05/eShop/domain.tests/CustomerTests.cs
@@ -0,0 +1,92 @@
+﻿namespace domain.tests;
+
+public class CustomerTests(CustomerFixture fixture) : IClassFixture<CustomerFixture>
+{
+    readonly CustomerFixture fixture = fixture;
+
+    [Fact]
+    public void ShouldCreateAnInstanceOfCustomer()
+    {
+        Assert.IsType<Customer>(fixture.Customer);
+    }
+
+    [Fact]
+    public void CustomerShouldHaveCorrectInitializedProperties()
+    {
+        Assert.Equal("", fixture.Customer.FirstName);
+        Assert.Equal("", fixture.Customer.LastName);
+        Assert.Equal("", fixture.Customer.Email);
+        Assert.Equal("", fixture.Customer.Phone);
+    }
+
+    [Fact]
+    public void SaveShouldReturnTrueWhenSuccessful()
+    {
+        // Act...
+        fixture.Customer.FirstName = "Michael";
+        fixture.Customer.LastName = "Gustavsson";
+        fixture.Customer.Email = "michael@gmail.com";
+
+        var result = fixture.Customer.Save();
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void SaveShouldThrowArgumentExceptionWhenDataIsMissing()
+    {
+        // Arrange...
+        var customer = new Customer
+        {
+            // Act...
+            FirstName = "Michael",
+            LastName = "Gustavsson"
+        };
+        // fixture.Customer.FirstName = "Michael";
+        // fixture.Customer.LastName = "Gustavsson";
+        // fixture.Customer.Email = "michael@gmail.com";
+        ArgumentException ex = Assert.Throws<ArgumentException>(()
+            => customer.Save());
+        // ArgumentException ex = Assert.Throws<ArgumentException>(()
+        //     => fixture.Customer.Save());
+
+        // Assert...
+        Assert.NotNull(ex);
+        Assert.IsType<ArgumentException>(ex);
+        Assert.Equal("Information saknas", ex.Message);
+    }
+
+    [Fact]
+    public void UpdateShouldReturnTrueWhenSuccessfull()
+    {
+        // Arrange...
+        var customer = new Customer
+        {
+            // Act...
+            LastName = "Olsson",
+            Email = "nils@gmail.com"
+        };
+        // Act...
+        // fixture.Customer.LastName = "Olsson";
+        // fixture.Customer.Email = "nils@gmail.com";
+        // var result = fixture.Customer.Update();
+        var result = customer.Update();
+
+        // Assert...
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void UpdateShouldThrowArgumentExceptionWhenUnsuccessful()
+    {
+        // Act...
+        fixture.Customer.LastName = "Olsson";
+        // fixture.Customer.Email = "nils@gmail.com";
+        ArgumentException ex = Assert.Throws<ArgumentException>(()
+            => fixture.Customer.Update());
+
+        // Assert...
+        Assert.NotNull(ex);
+        Assert.IsType<ArgumentException>(ex);
+        Assert.Equal("Information saknas", ex.Message);
+    }
+}
---
+++ Lektion-05/eShop/domain.tests/ProductTest.cs
@@ -0,0 +1,114 @@
+﻿using FluentAssertions;
+
+namespace domain.tests;
+
+public class ProductTests(ProductFixture fixture) : IClassFixture<ProductFixture>
+{
+    readonly ProductFixture fixture = fixture;
+
+    [Fact]
+    public void ShouldCreateProductInstanceWithCorrectInitializing()
+    {
+        // Arrange...
+        // Act...
+        // Assert...
+        Assert.IsType<Product>(fixture.Product);
+    }
+
+    [Fact]
+    public void ShouldHavePropertiesMatchingConstructorArguments()
+    {
+        // Arrange...
+        // Act...
+        // Assert...
+        Assert.Equal("AA-12345", fixture.Product.ItemNumber);
+        Assert.Equal("Spikpistol", fixture.Product.Name);
+        Assert.Equal(4995, fixture.Product.Price);
+    }
+
+    [Fact]
+    public void ShouldThrowArgumentExceptionWhenUpdateNameIsNull()
+    {
+        // Arrange...
+        // Act...
+        ArgumentException ex = Assert.Throws<ArgumentException>(() => fixture.Product.Update(itemNumber: "AA-12345", name: null, price: 4995));
+
+        // Assert...
+        Assert.NotNull(ex);
+        Assert.IsType<ArgumentException>(ex);
+        Assert.Equal("Namn saknas (Parameter 'name')", ex.Message);
+    }
+
+    [Fact]
+    public void ShouldThrowArgumentExceptionWhenUpdateNameIsEmpty()
+    {
+        // Arrange...       
+        // Act...
+        Action action = () => fixture.Product.Update(itemNumber: "AA-12345", name: "", price: 4995);
+        // Assert...
+        Assert.Throws<ArgumentException>(action);
+    }
+
+    [Theory]
+    [InlineData("S")]
+    [InlineData("Sp")]
+    [InlineData("Spi")]
+    [InlineData("Spik")]
+    public void ShouldThrowArgumentExceptionWhenUpdateNameHasInvalidCharacters(string name)
+    {
+        // Arrange...
+        // Act...
+        // Assert...
+        Action action = () => fixture.Product.Update(itemNumber: "AA-12345", name: name, price: 4995);
+        action.Should().Throw<ArgumentException>().WithMessage("Namn måste vara minst 5 tecken");
+    }
+
+    [Fact]
+    public void ShouldThrowArgumentExceptionWhenUpdateItemNumberIsNull()
+    {
+        // Arrange...        
+        // Act...
+        // Assert...
+        Assert.Throws<ArgumentException>(() => fixture.Product.Update(itemNumber: null, name: "Spikpistol", price: 4995));
+    }
+
+    [Fact]
+    public void ShouldThrowArgumentExceptionWhenUpdateItemNumberIsEmpty()
+    {
+        // Arrange...        
+        // Act...
+        // Assert...
+        Assert.Throws<ArgumentException>(() => fixture.Product.Update(itemNumber: "", name: "Spikpistol", price: 4995));
+    }
+
+    [Fact]
+    public void ShouldThrowArgumentExceptionWhenUpdateItemNumberHasInvalidCharacters()
+    {
+        // Arrange...        
+        // Act...
+        // Assert...
+        Assert.Throws<ArgumentException>(() => fixture.Product.Update(itemNumber: "AA-123", name: "Spikpistol", price: 4995));
+    }
+
+    [Fact]
+    public void ShouldThrowArgumentExceptionWhenUpdateHasNegativePrice()
+    {
+        // Arrange...
+        // Act...
+        // Assert...
+        Assert.Throws<ArgumentException>(() => fixture.Product.Update(itemNumber: "AA-12345", name: "Spikpistol", price: -1));
+    }
+
+    [Fact]
+    public void Save_ShouldReturnTrue_When_Successful() => fixture.Product.Save().Should().BeTrue();
+
+    // public void Save_ShouldReturnTrue_When_Successful()
+    // {
+    //     // Act...
+    //     var result = fixture.Product.Save();
+
+    //     // Assert...
+    //     Assert.True(result);
+    //     // Assert.Throws<ArgumentException>(() => fixture.Product.Save());
+    // }
+}
